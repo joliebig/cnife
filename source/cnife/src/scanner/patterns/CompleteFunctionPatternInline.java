91fba395eba9fe67198a66c6890a4b2efb95668c
Jörg Liebig <joliebig@fim.uni-passau.de>
2011-10-14 11:59:05 +0200

    wip

--- source/cnife/src/scanner/patterns/CompleteFunctionPatternInline.java
+++ source/cnife/src/scanner/patterns/CompleteFunctionPatternInline.java
@@ -4,6 +4,7 @@ import scanner.FeaturePattern;
 
 public class CompleteFunctionPatternInline extends FeaturePattern {
 	private static final String COMPLETE_FUNCTION_REFACTORING_INLINE = "CompleteFunctionRefactoringInline";
+	//private static final String COMPLETE_FUNCTIONS_INLINE = "/src:unit/(cpp:ifdef union cpp:ifndef)/following-sibling::src:function/following-sibling::cpp:endif/(. union preceding-sibling::cpp:ifdef union preceding-sibling::cpp:ifndef)";
 	private static final String COMPLETE_FUNCTIONS_INLINE = "/src:unit/(cpp:ifdef union cpp:ifndef)/following-sibling::src:function/following-sibling::cpp:endif/(. union preceding-sibling::cpp:ifdef union preceding-sibling::cpp:ifndef)";
 
 	public String getRefactoringType() {
